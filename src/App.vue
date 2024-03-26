b71c1d8640debc190ca377285b1ad01182be257d
Ahmad Badrul Ulum <ahmadbadrul635@gmail.com>
2024-03-26 10:16:08 +0700

    binding data ke component

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,40 @@
 <template>
-  <div id="app">
-    <h1>List Tugas</h1>
-    <ul>
-      <!-- <li v-for="task in tasks" :key="task.id">
-        <input type="checkbox" v-model="task.done">
-        <span :style="{ textDecoration: task.done ? 'line-through' : 'none' }">{{ task.name }}</span>
-      </li> -->
-      <li v-for="task in tasks" :key="task.id">{{ task.name }}</li>
-    </ul>
-
-  </div>
+    <div id="app">
+        <h1>List Tugas</h1>
+        <div v-for="task in tasks" :key="task.id">
+            <ItemTugas :task="task"></ItemTugas>
+        </div>
+        
+    </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import ItemTugas from './components/ItemTugas.vue'
 
 export default {
-  name: 'App',
-  data: function() {
-    return {
-      tasks: [
-        { id: 1, name: 'Membuat Aplikasi', done: false },
-        { id: 2, name: 'Membuat Presentasi', done: false },
-        { id: 3, name: 'Membuat Laporan', done: false }
-      ]
+    name: 'App',
+    data: function() {
+        return {
+            tasks: [
+                { id: 1, name: 'Membuat Aplikasi', done: false },
+                { id: 2, name: 'Membuat Presentasi', done: false },
+                { id: 3, name: 'Membuat Laporan', done: false }
+            ]
+        }
+    },
+    components: {
+        ItemTugas
     }
-  },
-  components: {}
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/ItemTugas.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <input type="checkbox">
+        <span>{{ task.name  }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ItemTugas',
+    props:['task']
+}
+</script>
