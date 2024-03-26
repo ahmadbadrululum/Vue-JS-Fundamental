96875f330a82a5f062d755bacd9ee963d31c5f8b
Ahmad Badrul Ulum <ahmadbadrul635@gmail.com>
2024-03-26 08:45:54 +0700

    vue component dan mengatur layouting

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App kenapa tiba2 disini"/> -->
+  <!-- example -->
+  <LogosGallery />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import LogosGallery from './components/LogosGallery.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LogosGallery
   }
 }
 </script>
---
+++ src/components/LogosGallery.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="logos-wrappper">
+        <h2 class="title-header">
+            Example Logos Component
+        </h2>
+        <div class="item-logos">
+            <img class="logo-baru" alt="Vue logo" src="../assets/logo.png">
+        </div>
+        <div class="item-logos">
+            <img class="logo-baru" alt="Vue logo" src="../assets/logo.png">
+        </div>
+        <div class="item-logos">
+            <img class="logo-baru" alt="Vue logo" src="../assets/logo.png">
+        </div>
+        <div class="item-logos">
+            <img class="logo-baru" alt="Vue logo" src="../assets/logo.png">
+        </div>
+        <div class="item-logos">
+            <img class="logo-baru" alt="Vue logo" src="../assets/logo.png">
+        </div>
+        <div class="item-logos">
+            <img class="logo-baru" alt="Vue logo" src="../assets/logo.png">
+        </div>
+        <div class="item-logos">
+            <img class="logo-baru" alt="Vue logo" src="../assets/logo.png">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'LogosGallery'
+}
+</script>
+
+<style scoped>
+.title-header {
+    text-align: left;
+    margin-bottom: 20px;
+}
+
+.logos-wrappper {
+    width: 80%;
+    overflow: hidden;
+    margin: 0 auto;
+}
+
+.item-logos{
+    float: left;
+    background: rgb(196, 224, 33);
+    width: 100px;
+    height: 100px;
+    margin: 10px;
+    border-radius: 20%;
+}
+
+.item-logos .logo-baru{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 20%;
+}
+</style>
